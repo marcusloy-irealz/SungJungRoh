e5737f1c08be4e114f795337a4ad7db8f4ab806e
Marcus Loy <marcus_loy@irealz.com>
2026-09-25 01:44:51 +0000

    chore: remove temporary script files

--- tmp/list_scripts.py
+++
@@ -1,7 +0,0 @@
-with open("./tmp/main_lock.html") as f:
-    html = f.read()
-
-import re
-scripts = re.findall(r'<script[^>]*src=["\']([^"\']+)["\']', html)
-for s in scripts:
-    print("Script src:", s)
